85677f8baddd1003a8d4349d50cb43764eec062d
pavelmezin <paulmezoni@gmain.com>
2022-03-03 12:29:23 +0300

    commit_2

---
+++ src/TaskCodeCombat/TwoSumEx.java
@@ -0,0 +1,68 @@
+package TaskCodeCombat;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class TwoSumEx {
+    public static void main(String[] args) throws InterruptedException {
+        var a = 6;
+        int[] nums = {1, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        List<Integer> list = List.of(1, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        System.out.println(Arrays.toString(twoSumHashSet(list, a)) + " = " + a + " twoSumHashSet");
+        System.out.println(Arrays.toString(twoSumTwoPoint(nums, a)) + " = " + a + " twoSumTwoPoint");
+        System.out.println(Arrays.toString(twoSumTwoFor(nums, a)) + " = " + a + " twoSumTwoFor");
+
+    }
+
+    // speed: O(n) memory: O(n)
+    static int[] twoSumHashSet(List<Integer> a, int k) {
+        Set<Integer> set = new HashSet<>();
+        for (int j : a) {
+            var numberToFind = k - j;
+            if (set.contains(numberToFind)) {
+                return new int[]{j, numberToFind};
+            } else {
+                set.add(j);
+            }
+        }
+        return new int[0];
+    }
+
+    // speed: O(n2) memory: O(1)
+    static int[] twoSumTwoFor(int[] nums, int target) {
+        for (int value : nums) {
+            for (var j = 1; j < nums.length; j++) {
+                if (target == value + nums[j]) {
+                    return new int[]{value, nums[j]};
+                }
+            }
+        }
+        return new int[0];
+    }
+
+    // speed: O(n) memory: O(1)
+    static int[] twoSumTwoPoint(int[] a, int k) {
+        int l = 0, r = a.length - 1;
+        while (l < r) {
+            var sum = a[l] + a[r];
+            if (sum == k) {
+                return new int[]{a[l], a[r]};
+            }
+            if (sum < k) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return new int[0];
+    }
+
+}
+
+
+
+
+
---
+++ src/stream/StreamPeek.java
@@ -0,0 +1,15 @@
+package stream;
+
+import java.util.stream.Stream;
+
+public class StreamPeek {
+    public static void main(String[] args) {
+
+        boolean b = Stream.of("a", "b", "c")
+                .peek(System.out::println)
+                .map(String::toUpperCase)
+                .peek(System.out::println)
+                .anyMatch("B"::equals);
+        System.out.println(b);
+    }
+}
